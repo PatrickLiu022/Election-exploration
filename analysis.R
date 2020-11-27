bad6d963ea12a7c4b0e5aed78f61d183b9c18f52
patrickliu022 <patrickliu022@gmail.com>
2020-11-26 21:31:34 -0800

    complete analysis

--- analysis.R
+++ analysis.R
@@ -22,9 +22,23 @@ timestamps_by_state <- raw_data %>%
 # as.Date("11/23/2020", format = "%m/%d/%Y")
 
 # Formatting: split out state name from electoral votes
+# Add biden and trump vote columns
 data <- raw_data %>% 
   separate(state, into = c("state", "ev"), " \\(") %>% 
-  mutate(ev = parse_number(ev))
+  mutate(ev = parse_number(ev)) %>% 
+  mutate(biden_votes = 
+           if_else(leading_candidate_name == "Biden", # condition
+                   leading_candidate_votes,   # if true 
+                   trailing_candidate_votes   # if false
+                  ),
+            trump_votes = total_votes_count - biden_votes
+)
+
+# Quick check!!! very useful
+data %>% 
+  mutate(total_check = trump_votes + biden_votes,
+         done_correctly = if_else(total_check == total_votes_count, 1, 0)) %>% 
+  summarise(total_correct = (done_correctly))
 
 # How many reported time stamps exist for each state
 
@@ -36,9 +50,28 @@ ga_lead_time <- data %>%
   pull(timestamp)
   
   
+# Earliest time in each state that biden is ahead?
+biden_lead_time <- data %>% 
+  group_by(state) %>% 
+  filter(leading_candidate_name == "Biden") %>% 
+  filter(timestamp == min(timestamp)) %>% 
+  select(state, timestamp)
 
 # What is the difference in votes in each state
+# at the most recent time stamp
+vote_diff <- data %>%
+  group_by(state) %>% 
+  filter(timestamp == max(timestamp)) %>% 
+  mutate(vote_diff = biden_votes - trump_votes,
+         pct_diff = vote_diff / total_votes_count)
 
+vote_diff_plot <- ggplot(vote_diff) +
+  geom_col(aes(x = vote_diff, 
+                         y = reorder(state, vote_diff),
+                         fill = leading_candidate_name)) +
+  scale_fill_manual(values = c("blue", "red")) +
+  labs(x = "Vote Difference" ,y = "State", fill = "Candidate", 
+       title = "Vote Difference at most recent time stamp")
 
 # How do total votes change over time (by candidate)
 
